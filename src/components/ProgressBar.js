a892262305fc3ea88b35fee040baf5fe8d70325d
Sharanu P <93566630+Sharanupatil8@users.noreply.github.com>
2023-12-04 01:20:59 +0530

    Update ProgressBar.js

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -6,10 +6,16 @@ function ProgressBar({
   questionNumber,
   totalQuestions,
 }) {
+  // Calculate the percentage of completion
+  const completionPercentage = (questionNumber / totalQuestions) * 100;
+
   return (
     <div className="flex flex-col mb-4">
-      <div class="mb-5 h-2 bg-slate-200">
-        <div class="h-2 bg-sky-500 w-3"></div>
+      <div className="mb-5 h-2 bg-slate-200">
+        <div
+          className="h-2 bg-sky-500"
+          style={{ width: `${completionPercentage}%` }}
+        ></div>
       </div>
       <div className="flex justify-between">
         <p>
