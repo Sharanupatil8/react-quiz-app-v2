b735d9bc340dc44ed169da85a24ce0b1c6b18daf
Sharanu P <93566630+Sharanupatil8@users.noreply.github.com>
2023-12-03 21:32:04 +0530

    Update WelcomeScreen.js

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -7,7 +7,7 @@ function WelcomeScreen({ setQuestionsReady }) {
       <h2 className="text-2xl md:text-4xl  font-bold">
         Welcome to The React Quiz!
       </h2>
-      <p className="text-xl mb-5 mt-4">15 questions to test your React Skill</p>
+      <p className="text-lg md:text-xl mb-5 mt-4">15 questions to test your React Skill</p>
       <button
         onClick={() => setQuestionsReady(true)}
         className="rounded transition duration-150 ease-linear hover:bg-slate-800 py-3 px-8 mt-4 text-lg bg-slate-300 text-slate-800 hover:text-slate-400 font-bold"
